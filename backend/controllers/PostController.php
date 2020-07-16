643c183ccf34d3e3cae97817ba719e9d67e026e1
Orlov Alexander <Gl-lab@live.ru>
2020-07-16 19:09:22 +0300

    Update PostController.php
    Не отметил один фаил в комите

--- backend/controllers/PostController.php
+++ backend/controllers/PostController.php
@@ -2,19 +2,73 @@
 
 
 namespace backend\controllers;
+use common\models\Post;
+use yii\rest\ActiveController;
+use common\models\User;
 
-
-class PostController extends \yii\rest\Controller
+class PostController extends ActiveController
 {
-    public $modelClass = 'backend\models\Post';
+    public $modelClass = 'Post';
 
-    public function actionPosts()
-    {
-        return ['xz'];
+    public function actions(){
+        $actions = parent::actions();
+        unset($actions['create']);
+        unset($actions['update']);
+        unset($actions['delete']);
+        unset($actions['view']);
+        unset($actions['index']);
+        return $actions;
     }
 
-    public function actionMyPosts()
+    public function actionNew()
     {
-        return ['ok'];
+        $request  = \Yii::$app->request;
+        $accessToken = $request->post('accessToken');
+        $text = $request->post('text');
+        if ($accessToken != null && $text != null){
+            $user = User::findIdentityByAccessToken($accessToken);
+            if ($user != null){
+                $post = new Post();
+                $post->author_id = $user->getId();
+                $post->title = 'title';
+                $post->body = $text;
+                $post->save();
+                return [];
+            }
+            return ['error' => 'Ошибка авторизации'];
+        }
+        return ['error' => 'Неверные данные'];
+    }
+
+    public function actionAll()
+    {
+        $request  = \Yii::$app->request;
+        $accessToken = $request->get('accessToken');
+        $offset = $request->get('offset',0);
+        $limit = $request->get('limit',10);
+        if ($accessToken != null){
+            $user = User::findIdentityByAccessToken($accessToken);
+            if ($user != null){
+                return Post::find()->limit($limit)->offset($offset)->all();
+            }
+            return ['error' => 'Ошибка авторизации'];
+        }
+        return ['error' => 'Неверные данные'];
+    }
+
+    public function actionMy()
+    {
+        $request  = \Yii::$app->request;
+        $accessToken = $request->get('accessToken');
+        $offset = $request->get('offset',0);
+        $limit = $request->get('limit',10);
+        if ($accessToken != null){
+            $user = User::findIdentityByAccessToken($accessToken);
+            if ($user != null){
+                return Post::find()->where('author_id = :user_id', [':user_id' => $user->id])->limit($limit)->offset($offset)->all();
+            }
+            return ['error' => 'Ошибка авторизации'];
+        }
+        return ['error' => 'Неверные данные'];
     }
 }
