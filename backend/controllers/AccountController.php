70707ae7e61d6eed5e038595a452973d3a62c401
Orlov Alexander <Gl-lab@live.ru>
2020-07-16 19:07:20 +0300

    Добавил API для добавления и просмотра постов

--- backend/controllers/AccountController.php
+++ backend/controllers/AccountController.php
@@ -38,4 +38,29 @@ class AccountController extends ActiveController
         }
         return $result;
     }
+
+    public function actionRegister()
+    {
+        $result = [];
+        $request  = \Yii::$app->request;
+        $name = $request->post('name');
+        $login = $request->post('login');
+        $password = $request->post('password');
+        if ($name != null && $login != null && $password != null){
+            $user = User::findByEmail($login);
+
+            if ($user != null) {
+                return ['error' => 'Пользователь уже существует'];
+            } else {
+                $user = new User();
+                $user->username = $name;
+                $user->email = $login;
+                $user->setPassword($password);
+                $user->generateAuthKey();
+                $user->save();
+                return $user->auth_key;
+            }
+        }
+        return ['error' => 'типа троу еррор'];
+    }
 }
